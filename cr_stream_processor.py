f2da89a77ae7287b4fb21a7a0b729e1d85d9a399
Mike Ames <61067224+mikames@users.noreply.github.com>
2022-05-16 11:53:50 -0700

    Add files via upload

---
+++ cr_stream_processor.py
@@ -0,0 +1,33 @@
+import boto3
+
+# add your ARNs here 
+KinesisVideoStreamARN = ""
+SNSTopicARN = ""
+
+client = boto3.client('rekognition')
+
+response = client.create_stream_processor(
+    Input={
+        'KinesisVideoStream': {
+            'Arn': KinesisVideoStreamARN
+        }
+    },
+    Output={
+        'S3Destination': {
+            'Bucket': 'tangelo-bucket',
+            'KeyPrefix': 'tang'
+        }
+    },
+    Name='tangelo1',
+        Settings = {'ConnectedHome': {
+            'Labels': ["PERSON", "PET", "PACKAGE","ALL"],
+            'MinConfidence': 90
+        }
+        },
+
+    RoleArn='arn:aws:iam::304243717290:role/RekTangelo',
+    NotificationChannel={
+        'SNSTopicArn': SNSTopicARN
+    }
+)
+print(response)
